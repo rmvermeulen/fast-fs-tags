8b8af1d38885f1f50cd9dd18f26ac356e3293d18
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-10-12 14:56:26 +0200

    Add skeleton scanner implementation

--- src/scanner/scanner.service.spec.ts
+++ src/scanner/scanner.service.spec.ts
@@ -1,18 +1,32 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { ScannerService } from './scanner.service';
+import { Test, TestingModule } from '@nestjs/testing'
+import { ScannerService } from './scanner.service'
 
 describe('ScannerService', () => {
-  let service: ScannerService;
+  let service: ScannerService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [ScannerService],
-    }).compile();
+    }).compile()
 
-    service = module.get<ScannerService>(ScannerService);
-  });
+    service = module.get<ScannerService>(ScannerService)
+  })
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
-});
+    expect(service).toBeDefined()
+  })
+
+  it('scans a directory to a given depth (WIP)', done => {
+    const obs = service.scan('.')
+
+    obs.subscribe({
+      next(info) {
+        console.log('next', info)
+      },
+      error(error) {
+        console.log('error', error)
+      },
+      complete: done,
+    })
+  })
+})
--- src/scanner/scanner.service.ts
+++ src/scanner/scanner.service.ts
@@ -1,4 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { readdir } from 'fs'
+import { EventEmitter } from 'events'
+import { fromEvent, Observable } from 'rxjs'
+import { scan } from 'rxjs/operators'
+
+type Info = {
+  path: string
+  directory: boolean
+}
+
+const getInfo = async (path: string): Promise<Info> => ({
+  path,
+  // todo: read
+  directory: false,
+})
 
 @Injectable()
-export class ScannerService {}
+export class ScannerService {
+  // file extensions to look for
+  private readonly extensions: string[] = ['png', 'bmp']
+  // scan a directory recursively
+  scan(path: string, depth = -1): Observable<Info> {
+    return new Observable(subscriber => {
+      // todo: use readdir paths
+      for (const path of ['a', 'b']) {
+        getInfo(path)
+          .then(subscriber.next)
+          .catch(subscriber.error)
+      }
+      subscriber.complete()
+    })
+  }
+}
