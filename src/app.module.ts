f16ae8a9eedc44df9dd140de0eb2da2ea07170d3
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-10-13 23:06:32 +0200

    Add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { CacheService } from './cache/cache.service';
-import { ScannerService } from './scanner/scanner.service';
-import { SqliteService } from './sqlite/sqlite.service';
+import { CacheService } from './cache/cache.service'
+import { ScannerService } from './scanner/scanner.service'
+import { SqliteService } from './sqlite/sqlite.service'
+import { AppLogger } from './app-logger/app-logger.service'
 
 import config from './config'
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    load: [config]
-  })],
-  providers: [CacheService, ScannerService, SqliteService],
+  imports: [
+    ConfigModule.forRoot({
+      load: [config],
+    }),
+  ],
+  providers: [CacheService, ScannerService, SqliteService, AppLogger],
 })
-export class AppModule { }
+export class AppModule {}
