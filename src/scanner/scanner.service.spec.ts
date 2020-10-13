43756a8127fdd2a1949b6f73663f68e913ee5c28
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-10-13 15:19:33 +0200

    Example of generator to get actual lazy, cancellable behaviour

--- src/scanner/scanner.service.spec.ts
+++ src/scanner/scanner.service.spec.ts
@@ -1,6 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing'
-import { map, partition, pluck, prop } from 'ramda'
-import { toArray } from 'rxjs/operators'
+import * as R from 'ramda'
+import { map, take, toArray } from 'rxjs/operators'
 import { Info, ScannerService } from './scanner.service'
 
 describe('ScannerService', () => {
@@ -18,20 +18,36 @@ describe('ScannerService', () => {
     expect(service).toBeDefined()
   })
 
-  it('scans a directory recursively', async () => {
-    const target = 'test/example-files'
-    const results: [Info[], Info[]] = await service
-      .scanPath(target)
-      .pipe(toArray())
-      .toPromise()
-      .then(partition(prop('isDirectory')))
-      .then(map(pluck('path')))
-    const [dirs, files] = results
-    expect(dirs).toEqual([`${target}/deep`, `${target}/deep/deeper`])
-    expect(files).toEqual([
-      `${target}/file1`,
-      `${target}/deep/file2`,
-      `${target}/deep/deeper/file3`,
-    ])
+  describe('scanning', () => {
+    it('scans a directory recursively', async () => {
+      const target = 'test/example-files'
+      const results: [Info[], Info[]] = await service
+        .scanPath(target)
+        .pipe(toArray())
+        .toPromise()
+        .then(R.partition(R.prop('isDirectory')))
+        .then(R.map(R.pluck('path')))
+      const [dirs, files] = results
+      expect(dirs).toEqual([`${target}/deep`, `${target}/deep/deeper`])
+      expect(files).toEqual([
+        `${target}/file1`,
+        `${target}/deep/file2`,
+        `${target}/deep/deeper/file3`,
+      ])
+    })
+
+    it('is lazy', async () => {
+      const results = []
+      const cb = jest.fn().mockImplementation(value => results.push(value))
+      const paths$ = service.scanPath('test/example-files').pipe(map(cb))
+      expect(cb).not.toHaveBeenCalled()
+      await paths$.pipe(take(3)).toPromise()
+      expect(cb).toHaveBeenCalledTimes(3)
+      await paths$.pipe(take(3)).toPromise()
+      expect(cb).toHaveBeenCalledTimes(6)
+      await paths$.pipe(take(3)).toPromise()
+      expect(cb).toHaveBeenCalledTimes(9)
+      expect(results).toEqual(R.uniq(results))
+    })
   })
 })
--- src/scanner/scanner.service.ts
+++ src/scanner/scanner.service.ts
@@ -1,54 +1,79 @@
 import { Injectable } from '@nestjs/common'
-import { stat, readdir } from 'fs'
-import { fromEvent, iif, Observable, of, merge } from 'rxjs'
-import { map, mergeMap, expand } from 'rxjs/operators'
+import { stat, readdir, promises } from 'fs'
+import { fromEvent, iif, Observable, of, merge, Subject } from 'rxjs'
+import { map, mergeMap, expand, publish, refCount } from 'rxjs/operators'
 import { join } from 'path'
+import { delay } from 'bluebird'
 
 export type Info = {
   path: string
   isDirectory: boolean
 }
 
-const readdirObservable = (dir: string): Observable<string> =>
-  new Observable(sub => {
-    readdir(dir, (error, files) => {
-      if (error) {
-        return sub.error(error)
-      }
-      files.map(name => join(dir, name)).forEach(path => sub.next(path))
-      sub.complete()
-    })
-  })
+async function* recursePath(path: string) {
+  yield path
+  try {
+    const names = await promises.readdir(path)
+    for (const name of names) {
+      yield* recursePath(join(path, name))
+    }
+  } catch {
+    return
+  }
+}
+;(async () => {
+  const it = recursePath('test/example-files')
 
-const getInfo = (path: string): Observable<Info> =>
-  new Observable(sub => {
-    stat(path, (error, stats) => {
-      if (error) {
-        return sub.error(error)
-      }
-      sub.next({
-        path,
-        isDirectory: stats.isDirectory(),
-      })
-      sub.complete()
-    })
-  })
-
-const scanPath = (path: string): Observable<Info> =>
-  readdirObservable(path).pipe(
-    mergeMap(getInfo),
-    mergeMap((info: Info) =>
-      // return info as-is, if it's a directory add entries
-      info.isDirectory ? merge(of(info), scanPath(info.path)) : of(info),
-    ),
-  )
-
+  for (let i = 0; i < 15; ++i) {
+    if (i % 3 == 0) {
+      await delay(1000)
+    }
+    const { value } = await it.next()
+    if (!value) {
+      break
+    }
+    console.log(i, value)
+  }
+})()
 @Injectable()
 export class ScannerService {
   // file extensions to look for
   private readonly extensions: string[] = ['png', 'bmp']
   // scanPath a directory recursively
-  scanPath(path: string): Observable<Info> {
-    return scanPath(path)
+  public scanPath(path: string): Observable<Info> {
+    // return this._scanPath(path).pipe(publish(), refCount())
+    return merge(new Subject(), this._scanPath(path))
   }
+  private _scanPath = (path: string): Observable<Info> =>
+    this._readdirObservable(path).pipe(
+      mergeMap(this._getInfo),
+      mergeMap((info: Info) =>
+        // return info as-is, if it's a directory add entries
+        info.isDirectory ? merge(of(info), this.scanPath(info.path)) : of(info),
+      ),
+    )
+  private _readdirObservable = (dir: string): Observable<string> =>
+    new Observable(sub => {
+      readdir(dir, (error, files) => {
+        if (error) {
+          return sub.error(error)
+        }
+        files.map(name => join(dir, name)).forEach(path => sub.next(path))
+        sub.complete()
+      })
+    })
+
+  private _getInfo = (path: string): Observable<Info> =>
+    new Observable(sub => {
+      stat(path, (error, stats) => {
+        if (error) {
+          return sub.error(error)
+        }
+        sub.next({
+          path,
+          isDirectory: stats.isDirectory(),
+        })
+        sub.complete()
+      })
+    })
 }
